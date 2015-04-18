31ebe948a09afdf8e31ac257fde9ef64f757af06
Gary Burgess <gary.burgess@gmail.com>
2015-04-18 23:42:04 +0100

    Update docs, build, dependencies

--- src/Data/Profunctor.purs
+++ src/Data/Profunctor.purs
@@ -1,35 +1,35 @@
 module Data.Profunctor where
 
-  -- | A `Profunctor` is a `Functor` from the pair category `(Type^op, Type)`
-  -- | to `Type`.
-  -- |
-  -- | In other words, a `Profunctor` is a type constructor of two type
-  -- | arguments, which is contravariant in its first argument and covariant
-  -- | in its second argument.
-  -- | 
-  -- | The `dimap` function can be used to map functions over both arguments
-  -- | simultaneously.
-  -- |
-  -- | A straightforward example of a profunctor is the function arrow `(->)`.
-  -- |
-  -- | Laws:
-  -- | 
-  -- | - Identity: `dimap id id = id`
-  -- | - Composition: `dimap f1 g1 <<< dimap f2 g2 = dimap (f1 >>> f2) (g1 <<< g2)`
-  class Profunctor p where
-    dimap :: forall a b c d. (a -> b) -> (c -> d) -> p b c -> p a d
+-- | A `Profunctor` is a `Functor` from the pair category `(Type^op, Type)`
+-- | to `Type`.
+-- |
+-- | In other words, a `Profunctor` is a type constructor of two type
+-- | arguments, which is contravariant in its first argument and covariant
+-- | in its second argument.
+-- |
+-- | The `dimap` function can be used to map functions over both arguments
+-- | simultaneously.
+-- |
+-- | A straightforward example of a profunctor is the function arrow `(->)`.
+-- |
+-- | Laws:
+-- |
+-- | - Identity: `dimap id id = id`
+-- | - Composition: `dimap f1 g1 <<< dimap f2 g2 = dimap (f1 >>> f2) (g1 <<< g2)`
+class Profunctor p where
+  dimap :: forall a b c d. (a -> b) -> (c -> d) -> p b c -> p a d
 
-  -- | Map a function over the (contravariant) first type argument only.
-  lmap :: forall a b c p. (Profunctor p) => (a -> b) -> p b c -> p a c
-  lmap a2b = dimap a2b id
+-- | Map a function over the (contravariant) first type argument only.
+lmap :: forall a b c p. (Profunctor p) => (a -> b) -> p b c -> p a c
+lmap a2b = dimap a2b id
 
-  -- | Map a function over the (covariant) second type argument only.
-  rmap :: forall a b c p. (Profunctor p) => (b -> c) -> p a b -> p a c
-  rmap b2c = dimap id b2c
+-- | Map a function over the (covariant) second type argument only.
+rmap :: forall a b c p. (Profunctor p) => (b -> c) -> p a b -> p a c
+rmap b2c = dimap id b2c
 
-  -- | Lift a pure function into any `Profunctor` which is also a `Category`.
-  arr :: forall a b p. (Category p, Profunctor p) => (a -> b) -> p a b
-  arr f = rmap f id
+-- | Lift a pure function into any `Profunctor` which is also a `Category`.
+arr :: forall a b p. (Category p, Profunctor p) => (a -> b) -> p a b
+arr f = rmap f id
 
-  instance profunctorArr :: Profunctor (->) where
-    dimap a2b c2d b2c = a2b >>> b2c >>> c2d
+instance profunctorArr :: Profunctor (->) where
+  dimap a2b c2d b2c = a2b >>> b2c >>> c2d
--- src/Data/Profunctor/Strong.purs
+++ src/Data/Profunctor/Strong.purs
@@ -1,37 +1,36 @@
 module Data.Profunctor.Strong where
 
-  import Data.Profunctor
-  import Data.Tuple (Tuple(..))
+import Data.Profunctor
+import Data.Tuple (Tuple(..))
 
-  -- | The `Strong` class extends `Profunctor` with combinators for working with
-  -- | product types.
-  -- |
-  -- | `first` and `second` lift values in a `Profunctor` to act on the first and
-  -- | second components of a `Tuple`, respectively.
-  -- |
-  class (Profunctor p) <= Strong p where
-    first :: forall a b c. p a b -> p (Tuple a c) (Tuple b c)
-    second :: forall a b c. p b c -> p (Tuple a b) (Tuple a c)
+-- | The `Strong` class extends `Profunctor` with combinators for working with
+-- | product types.
+-- |
+-- | `first` and `second` lift values in a `Profunctor` to act on the first and
+-- | second components of a `Tuple`, respectively.
+class (Profunctor p) <= Strong p where
+  first :: forall a b c. p a b -> p (Tuple a c) (Tuple b c)
+  second :: forall a b c. p b c -> p (Tuple a b) (Tuple a c)
 
-  instance strongArr :: Strong (->) where
-    first a2b (Tuple a c) = Tuple (a2b a) c
-    second = (<$>)
+instance strongArr :: Strong (->) where
+  first a2b (Tuple a c) = Tuple (a2b a) c
+  second = (<$>)
 
-  infixr 3 ***
-  infixr 3 &&&
+infixr 3 ***
+infixr 3 &&&
 
-  -- | Compose a value acting on a `Tuple` from two values, each acting on one of
-  -- | the components of the `Tuple`.
-  (***) :: forall p a b c d. (Category p, Strong p) => p a b -> p c d -> p (Tuple a c) (Tuple b d)
-  (***) l r = first l >>> second r
+-- | Compose a value acting on a `Tuple` from two values, each acting on one of
+-- | the components of the `Tuple`.
+(***) :: forall p a b c d. (Category p, Strong p) => p a b -> p c d -> p (Tuple a c) (Tuple b d)
+(***) l r = first l >>> second r
 
-  -- | Compose a value which introduces a `Tuple` from two values, each introducing
-  -- | one side of the `Tuple`.
-  -- |
-  -- | This combinator is useful when assembling values from smaller components,
-  -- | because it provides a way to support two different types of output.
-  (&&&) :: forall p a b c. (Category p, Strong p) => p a b -> p a c -> p a (Tuple b c)
-  (&&&) l r = split >>> (l *** r)
-    where
-    split :: p a (Tuple a a)
-    split = dimap id (\a -> Tuple a a) id
+-- | Compose a value which introduces a `Tuple` from two values, each introducing
+-- | one side of the `Tuple`.
+-- |
+-- | This combinator is useful when assembling values from smaller components,
+-- | because it provides a way to support two different types of output.
+(&&&) :: forall p a b c. (Category p, Strong p) => p a b -> p a c -> p a (Tuple b c)
+(&&&) l r = split >>> (l *** r)
+  where
+  split :: p a (Tuple a a)
+  split = dimap id (\a -> Tuple a a) id
