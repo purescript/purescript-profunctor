7ad07580d039ac65e52c089ea1093e6cd5e748b5
Lukas Heidemann <lukasheidemann@gmail.com>
2016-01-02 15:14:02 +0100

    Closed, Cochoice and Costrong.
    In preparation of a proper `re` and `Grate`s in
    profunctor-lenses.

---
+++ src/Data/Profunctor/Costrong.purs
@@ -0,0 +1,11 @@
+module Data.Profunctor.Cochoice where
+
+import Prelude
+
+import Data.Either (Tuple ())
+import Data.Profunctor
+
+-- | The `Costrong` class provides the dual operations of the `Strong` class.
+class (Profunctor p) <= Costrong p where
+  unfirst :: forall a b c. p (Tuple a c) (Tuple b c) -> p a b
+  unsecond :: forall a b c. p (Tuple a b) (Tuple a c) -> p b c
