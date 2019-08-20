be868fc0c46df3b91cf15e64391d605fd064ee11
Asad Saeeduddin <masaeedu@gmail.com>
2019-08-20 03:02:35 -0400

    Add Monad instance for Joker

--- src/Data/Profunctor/Joker.purs
+++ src/Data/Profunctor/Joker.purs
@@ -3,7 +3,7 @@ module Data.Profunctor.Joker where
 import Prelude
 
 import Data.Either (Either(..))
-import Data.Newtype (class Newtype)
+import Data.Newtype (class Newtype, un)
 import Data.Profunctor (class Profunctor)
 import Data.Profunctor.Choice (class Choice)
 
@@ -26,6 +26,17 @@ instance profunctorJoker :: Functor f => Profunctor (Joker f) where
 instance clownJoker :: Functor f => Choice (Joker f) where
   left  (Joker f) = Joker $ map Left f
   right (Joker f) = Joker $ map Right f
+
+instance applyJoker :: Apply f => Apply (Joker f a) where
+  apply (Joker f) (Joker g) = Joker $ apply f g
+
+instance applicativeJoker :: Applicative f => Applicative (Joker f a) where
+  pure = Joker <<< pure
+
+instance bindJoker :: Bind f => Bind (Joker f a) where
+  bind (Joker ma) amb = Joker $ ma >>= (amb >>> un Joker)
+
+instance monadJoker :: Monad m => Monad (Joker m a)
 
 hoistJoker :: forall f g a b. (f ~> g) -> Joker f a b -> Joker g a b
 hoistJoker f (Joker a) = Joker (f a)
